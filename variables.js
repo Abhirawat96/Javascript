8d3ec97e67bfd408222624457b3c7f9208674af0
Abhishek kumar rawat <119392775+Abhirawat96@users.noreply.github.com>
2023-08-08 00:06:37 +0530

    Add files via upload
    Variables Declaration and exercise on variables

---
+++ variables.js
@@ -0,0 +1,68 @@
+/// javascript Data types//
+//1. Number
+
+let a = 5;
+let b = 6;
+const sum = a + b;
+console.log(sum);
+
+//2. String
+
+let name = "ram";
+let job = "developer";
+
+let profile = `the name of ${name} andhe is a ${job}`
+ 
+//3. Boolean 
+ 
+let num = 5;
+let num1 = 6;
+const bool = 5<6;
+console.log(bool);
+
+3 !== 3 //False
+4 !==5 //true  
+
+//Exercise of data types
+
+//Evaluate the below:
+// 5 + "34" = '534'
+// 5 - "4"  = 1
+// 10 % 5   = 0
+// 5 % 10   =5
+// "Java" + "Script" = javascript
+// " " + " "  = ''
+// " " + 0    = '0'
+// true + true  = 2
+// true + false  = 1
+// false + true  = 1
+// false - true  = -1
+// 3 - 4        = -1
+// "Bob" - "bill"  =nan
+
+
+//Evaluate the below comparisons:
+// 5 >= 1 = true
+// 0 === 1 =  false
+// 4 <= 1 =  false
+// 1 != 1 = false
+// "A" > "B" = false
+// "B" < "C" = true
+// "a" > "A" = true
+// "b" < "A" = false
+// true === false = false
+// true != true = false
+
+
+// Make the string: "Hi There! It's "sunny" out" by using the + sign:
+
+// "hi there! + "it'\s \"sunny\" out"
+
+
+
+
+
+
+//4. undefined
+
+    
