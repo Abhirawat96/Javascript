0b91345221e3181e2387478d0858498b00f07521
Abhishek kumar rawat <119392775+Abhirawat96@users.noreply.github.com>
2023-08-08 15:32:47 +0530

    Add files via upload
    js terminology :
    creating fucntion,function expression,method, expression,assign a variables

---
+++ js terminolgy.js
@@ -0,0 +1,39 @@
+//function declaration
+
+function newfunction(){
+
+}
+
+
+//function expresssion//
+var newfunction = function(){
+
+}
+
+
+//expresssion// is that something which return value
+
+1+3;
+var a = 2;
+return  true;
+
+
+//calling and invoking function//
+
+alert();
+newfunction(param1,param2);
+
+//assign a variable
+var a = true;
+
+//function vs method
+function myfunction(){
+
+}
+var obj = {
+    thisismethod:function(){
+
+    }
+}
+thisismethod()//function
+obj.thisismethod() //method
